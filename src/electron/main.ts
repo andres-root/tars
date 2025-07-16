01201d97bdc95b78cad8388a31d1409b3b0a71f1
andres-root <andreslujandev@gmail.com>
2025-07-16 17:43:50 -0500

    Add memory and cpu usage

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import { isDev } from './utils.js'
+import { pollResources } from './resourceManager.js'
 
 type test = string;
 
@@ -14,4 +15,5 @@ app.on('ready', () => {
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'))
   }
+  pollResources()
 })
