1567d74c5953fd07f36ab1982f7f2312a2c5b2c9
andres-root <andreslujandev@gmail.com>
2025-07-16 17:10:08 -0500

    Init repo

---
+++ src/electron/main.ts
@@ -0,0 +1,12 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+type test = string;
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'))
+})
